ad7871b90b7bcb9927c36b0cf02375143a831925
jjacobowitz <57004711+jjacobowitz@users.noreply.github.com>
2021-10-25 17:50:26 -0400

    Many changes
    * Revamped the section comments
    * removed Pandas warnings (caused by statsmodels) from not converting the dataframes into numpy arrays
    * separated the stata and normal summary table generation
    * stopped from always plotting the regression; only do it if all the required variables are passed
    * Transposed the describe result (Loujaina said the transpose of what I had is more common)
    * Added parts 2c, 2d, 2f
    * Modified part 2e

--- HW3/SS340_HW3.py
+++ HW3/SS340_HW3.py
@@ -7,6 +7,9 @@ Created: 10/23/2021
 Due: 11/01/2021
 
 Python program for Homework 3
+
+Note: The order of operations do not match the HW, so Part 1d might come before
+part 1a
 """
 import numpy as np
 import pandas as pd
@@ -15,52 +18,74 @@ from statsmodels.iolib.summary2 import summary_col
 from scipy.stats import t, ttest_ind
 import matplotlib.pyplot as plt
 
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 # close any currently open plots
 plt.close("all")
 
+# wipe the summary files
+with open("normal_summary.txt", "w") as f:
+    f.truncate()
+
 # wipe the summary file
-with open("summary.txt", "w") as f:
+with open("stata_summary.txt", "w") as f:
     f.truncate()
 
 # =============================================================================
 # Useful Functions
 # =============================================================================
-def run_regression(x, y, xlabel, ylabel, title, save_title):
+def run_regression(x, y, 
+                   xlabel=None, ylabel=None, title=None, save_title=None):
     # add the x data to the model
-    x_sm = sm.add_constant(x)
+    X = sm.add_constant(x)
     
     # fit the model using robust regression
-    model = sm.OLS(y, x_sm).fit(cov_type="HC1")
-    print(model.summary())
+    model = sm.OLS(y, X).fit(cov_type="HC1")
     
-    # create and print the stata-style summary table; save to a txt
-    stata_summary = summary_col(model, stars=True, float_format='%0.2f')
-    print(stata_summary)
-    with open('summary.txt', 'a') as f:
+    if None not in [xlabel, ylabel, title, save_title]:
+        # add new x data for the plot fit line
+        x_cont = np.linspace(x.min(), x.max(), 1000)
+        X = sm.add_constant(x_cont)
+        
+        # generate the regression y data
+        y_fit = model.predict(X)
+    
+        # plot the data with the regression line
+        plt.figure()
+        plt.scatter(x, y, marker='.', label="data")
+        plt.plot(x_cont, y_fit, 'r', label="fit")
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+        plt.savefig(save_title)
+    
+    return model, model.params, model.pvalues[1]
+
+def get_normal_model_summary(model, title):
+    # create the normal-style summary table and save to a txt
+    normal_summary = model.summary()
+    with open('normal_summary.txt', 'a') as f:
         f.write(title)
+        f.write('\n')
+        f.write(normal_summary.as_text())
+        f.write("\n\n\n")
+        
+    return normal_summary
+
+def get_stata_model_summary(model, name, title):    
+    # create the stata-style summary table and save to a txt
+    stata_summary = summary_col(model, model_names=name, stars=True, float_format="%0.2f")
+    with open('stata_summary.txt', 'a') as f:
+        f.write(title)
+        f.write('\n')
         f.write(stata_summary.as_text())
         f.write("\n\n\n")
-    
-    # add new x data for the plot fit line
-    x_cont = np.linspace(x.min(), x.max(), 1000)
-    x_cont_sm = sm.add_constant(x_cont)
-    
-    # generate the regression y data
-    y_fit = model.predict(x_cont_sm)
-    
-    # plot the data with the regression line
-    plt.figure()
-    plt.scatter(x, y, marker='.', label="data")
-    plt.plot(x_cont, y_fit, 'r', label="fit")
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-    plt.savefig(save_title)
-    
-    return model.params, model.pvalues[1], stata_summary
+        
+    return stata_summary
 
 def confidence(alpha, single_tailed=True):
     """calculates the single- or double-tailed confidence"""
@@ -87,7 +112,7 @@ def null_hypothesis_test(p_val, alpha):
         print(f"Fail to reject the null hypothesis with alpha = {alpha*100}%")
 
 # =============================================================================
-# Read data and clean it
+# Read in data
 # =============================================================================
 cols = ["dmage", "mrace3", "dmar", "dlivord", "frace4", "dgestat", "csex", 
         "dbrwt", "dplural", "cigar", "drink", "tobacco", "wgain", "lung", 
@@ -95,6 +120,9 @@ cols = ["dmage", "mrace3", "dmar", "dlivord", "frace4", "dgestat", "csex",
 
 data = pd.read_csv("pennbirthwgt0.csv", usecols=cols)
 
+# =============================================================================
+# Rename columns (Part 1d)
+# =============================================================================
 # new column names
 rename = ["agemother", "whitemother", "married", "livebirths", 
           "whitefather", "gestationweek", "male", "birthweight", "kidsatonce", 
@@ -103,6 +131,9 @@ rename = ["agemother", "whitemother", "married", "livebirths",
 
 data.rename(columns={old:new for old, new in zip(cols, rename)}, inplace=True)
 
+# =============================================================================
+# Remove missing (Parts 1a and 1c)
+# =============================================================================
 # the values for missing data for each variable
 missing = {"livebirths":99,
            "whitefather":4,
@@ -123,6 +154,9 @@ for k, v in missing.items():
     else:
         data.drop(data[data[k] == v].index, inplace=True)
 
+# =============================================================================
+# Correct dummy variables (Part 1b)
+# =============================================================================
 # convert boolean columns to 1 or 0 by replacing not 1s with 0s
 booleans = ["whitemother", "married", "whitefather", "male", "tobaccouse", 
             "lungdisease", "cardiacdisease"]
@@ -130,22 +164,24 @@ for boolean in booleans:
     data[boolean].replace([2, 3], 0, inplace=True)
     
 data["smoker"] = data["cigsperday"] > 0
+smoker = data["smoker"]         # easier reference to the smoker dummy variable
 
 # =============================================================================
 # Summary Statistics (Part 1e)
 # =============================================================================
 important_statistics = ["mean", "std", "min", "max"]
-data_describe = data.describe().loc[important_statistics].round(2)
-data_describe.loc[important_statistics].to_csv("SS340_HW3_Describe.csv")
+data_describe = data.describe().loc[important_statistics].transpose().round(2)
+data_describe[important_statistics].to_csv("SS340_HW3_Describe.csv")
 print(data_describe)    
 
 # =============================================================================
 # Mean Difference in Birthweight by Smoking (Parts 2a and 2b)
 # =============================================================================
-mean_birthweight_notsmoker = data["birthweight"][~data["smoker"]].mean()
-mean_birthweight_smoker = data["birthweight"][data["smoker"]].mean()
+mean_birthweight_nonsmoker = data["birthweight"][~smoker].mean()
+mean_birthweight_smoker = data["birthweight"][smoker].mean()
 
-print(f"Mean birthweight not-smoker: {mean_birthweight_notsmoker:.2f}g")
+print("\n")
+print(f"Mean birthweight not-smoker: {mean_birthweight_nonsmoker:.2f}g")
 print(f"Mean birthweight smoker: {mean_birthweight_smoker:.2f}g")
 
 # hypothesis: average birthweight is similar for smokers and non-smokers
@@ -157,29 +193,105 @@ n = len(data)
 dof = n - 2     # -2 because there are two means used
 
 # mean-mean null hypothesis
-t_stat, p_val = ttest_ind(data["birthweight"][~data["smoker"]], 
-                          data["birthweight"][data["smoker"]], 
+t_stat, p_val = ttest_ind(data["birthweight"][~smoker], 
+                          data["birthweight"][smoker], 
                           equal_var=False)
 
+print("\n")
 print("birthweight-smoker null hypothesis test:", end=" ")
 null_hypothesis_test(p_val, alpha)
 t_tab = t_tabulated(conf, dof)
 print(f"{t_stat=:.2f}, {t_tab=:.2f}, {p_val=:.2f}")
 
 # =============================================================================
+# Table of differences (Part 2c)
+# =============================================================================
+# variables we think might show the populations of mothers are not comparable
+of_interest = ["birthweight", "agemother", "whitemother", "married", 
+               "livebirths", "gestationweek", "kidsatonce", "etohperweek", 
+               "pregweightgain", "cardiacdisease"]
+
+# skeleton of the table with the desired columns
+table_of_diffs = pd.DataFrame(columns=["Smoker", 
+                                       "Not Smoker", 
+                                       "Difference", 
+                                       "p-value"])
+
+table_of_diffs.index.name = "Variable"  # rename the index column to variable
+
+# Creates a table showing the values for each group, their differences, and 
+# the p-value, where the null hypothesis is that they are equal. If the null
+# hypothesis is rejected (p<0.05) then there is a difference, and the groups
+# are not equal
+for interest in of_interest:
+    smoker_data = data[interest][smoker]
+    nonsmoker_data = data[interest][~smoker]
+    smoker_result = smoker_data.mean()
+    nonsmoker_result = nonsmoker_data.mean()
+    diff = smoker_result - nonsmoker_result
+    _, p_value = ttest_ind(smoker_data, nonsmoker_data, equal_var=False)
+    row = {"Smoker":smoker_result, 
+           "Not Smoker":nonsmoker_result, 
+           "Difference":diff, 
+           "p-value":round(p_value,2)}  # round to 2 decimals for clarity
+    row = pd.Series(row, name=interest)
+    table_of_diffs = table_of_diffs.append(row)
+    
+# =============================================================================
+# Correlation matrix (Part 2d)
+# =============================================================================
+# DataFrame.corr() creates a correlation matrix of the DataFrame
+corr = data[of_interest].corr()
+print("\n")
+print(corr.round(2))
+
+# =============================================================================
 # Simple Regression (Part 2e)
 # =============================================================================
 print("\nSimple Regression")
-result = run_regression(data["cigsperday"].to_numpy(),
-                        data["birthweight"].to_numpy(),
-                        "Cigarettes Smoked [cigs./day]",
-                        "Child Birthweight [g]",
-                        "Birthweight vs. Cigarettes Smoked Per Day",
-                        "SS340_HW3_simpleregress.png")
+title = "Birthweight vs. Cigarettes Smoked Per Day"
+xlabel = "Cigarettes Smoked [cigs./day]"
+ylabel = "Child Birthweight [g]"
+result = run_regression(data["cigsperday"],
+                        data["birthweight"],
+                        xlabel=xlabel,
+                        ylabel=ylabel,
+                        title=title,
+                        save_title="SS340_HW3_simpleregress.png")
 
-[beta_0, beta_1], p_val, stata_summary = result
+model, [beta_0, beta_1], p_val = result
+normal_summary = get_normal_model_summary(model, title)
+stata_summary = get_stata_model_summary(model, "birthweight", title)
+
 
 print(f"{beta_0=:.2f}, {beta_1=:.2f}")
 print("birthweight-cigs beta_0 null hypothesis test:", end=" ")
 null_hypothesis_test(p_val, alpha)
-    
+
+# =============================================================================
+# Adding more covariates (Part 2f)
+# =============================================================================
+covariates = ["cigsperday", 
+              "whitemother", 
+              "married", 
+              "livebirths", 
+              "pregweightgain",                 # until here: good controls
+              "agemother", 
+              "whitefather", 
+              "male",  
+              "kidsatonce", 
+              "etohperweek",                    # until here: useless controls
+              "gestationweek",
+              "tobaccouse",  
+              "lungdisease", 
+              "cardiacdisease"]                 # until here: bad controls
+models = []
+names = []
+for indx, covariate in enumerate(covariates):
+    xdata = data[covariates[:indx+1]]
+    ydata = data["birthweight"]
+    model, _, _ = run_regression(xdata, ydata)
+    models.append(model)
+    names.append(f"({indx})")
+
+stata_summary = get_stata_model_summary(models, names, "Testing Covariates")
