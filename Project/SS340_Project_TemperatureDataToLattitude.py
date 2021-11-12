8697132e18481bfa59b96fe67250cee6e7a3df97
jjacobowitz <57004711+jjacobowitz@users.noreply.github.com>
2021-11-12 17:38:26 -0500

    track failed fips

--- Project/SS340_Project_TemperatureDataToLattitude.py
+++ Project/SS340_Project_TemperatureDataToLattitude.py
@@ -17,39 +17,44 @@ def get_latlong(county, state, country="US"):
     return loc.latitude, loc.longitude
 
 def get_county_state(fip, fips2cands):
-    if fip not in fips2cands.fips:
-        return "", ""
+    if fips2cands[fips2cands.fips == fip].size == 0:
+        return "",""
     county = fips2cands[fips2cands.fips == fip]["county"].values[0]
     state = fips2cands[fips2cands.fips == fip]["state"].values[0]
     
     return county, state
 
 # data of the fip code, county, and state
-fips2cands = pd.read_csv("Fips2CountyandState.csv", dtype=str)
+fips2cands = pd.read_csv("Fips2CountyandState.csv")
 fips2cands.rename(columns={"FIPS":"fips", "Name":"county", "State":"state"}, 
                   inplace=True)
+# fips2cands.fips = fips2cands.fips.astype(str)
 
 # data of the fip code, year, temperature in F, and temperature in C
 data = pd.read_csv("TemperatureData.csv")
+failed = []
 
 # keep track of previous fip (they are sorted) to speed up the process
 # print out the fip, county, and state if get_longlat fails
 prev_fip = ""
 for row in data.itertuples():
     indx = row.Index
-    fip = str(int(row.fips))
-    if fip in ["5081", "5059"]:     # these fail for some reason
+    fip = int(row.fips)
+    # these fail for some reason
+    if fip in [5059, 5081, 9009, 17161, 18089, 19021, 22075]:           
         continue
-    if row.lat != 0.:
-        # print(indx, row["lat"])
+    # skip already filled rows
+    if not pd.isnull(row.county) and row.county != "":
         continue
     if fip != prev_fip:
         county, state = get_county_state(fip, fips2cands)
+        # if county
         try:
             lat, long = get_latlong(county, state)
         except AttributeError:
             # this happens when get_latlong fails to find the location
-            print(fip, county, state)
+            print(indx, fip, county, state)
+            failed.append((indx, fip, county, state))
             lat, long = None, None
         # geopy has a custom timeout error
         # this catches that and saves the data so the code can be run again
@@ -64,4 +69,8 @@ for row in data.itertuples():
                                                               long)
 print(indx)
 data.to_csv("TemperatureData.csv", index=False)
+with open("failed.txt", "w") as f:
+    f.write("indx,fip,county,state\n")
+    for fail in failed:
+        f.write(",".join(str(a) for a in fail)+'\n')
     
