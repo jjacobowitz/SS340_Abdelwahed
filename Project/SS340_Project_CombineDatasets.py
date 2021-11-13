a0e61647ad514098ccd89f43a8b8d10199874cd6
jjacobowitz <57004711+jjacobowitz@users.noreply.github.com>
2021-11-13 18:46:29 -0500

    Create SS340_Project_CombineDatasets.py
    Combine datasets by adding a new column to the temperature dataset that shows the number of disasters for that year. Counts statewide disasters for all counties in that state.

---
+++ Project/SS340_Project_CombineDatasets.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Jared Jacobowitz
+Fall 2021
+SS340 Cause and Effect
+Final Project
+
+Combine the temperature and disaster datasets
+"""
+import pandas as pd
+# import numpy as np
+
+disaster_data = pd.read_csv("DisasterData.csv")
+temps_data = pd.read_csv("TemperatureData.csv")
+temps_data.dropna(inplace=True)
+
+# max of mins is the smallest common value
+# min of maxes is the largest common value
+min_year = max(min(disaster_data["year"]), min(temps_data["year"]))
+max_year = min(max(disaster_data["year"]), max(temps_data["year"]))
+
+temps_data = temps_data[(temps_data["year"] <= max_year)
+                        & (temps_data["year"] >= min_year)]
+disaster_data = disaster_data[(disaster_data["year"] <= max_year)
+                              & (disaster_data["year"] >= min_year)]
+
+temps_data["disasters"] = 0
+
+for row in disaster_data.itertuples():
+    year = row.year
+    indx = temps_data["year"] == year
+    if row.statewide:
+        indx &= (temps_data["state"] == row.state)
+    else:
+        indx &= (temps_data["fips"] == row.fips)
+    temps_data.loc[indx, "disasters"] += 1
+    
+temps_data.to_csv("CombinedData.csv", index=False)
