2bed070498b653306a3170ef7280f029e68e24a5
jjacobowitz <57004711+jjacobowitz@users.noreply.github.com>
2021-11-13 18:45:34 -0500

    Create SS340_Project_ProcessDisasterDataset.py
    Creates a new csv with combined fips and dummy variable whether the incident was statewide or not

---
+++ Project/SS340_Project_ProcessDisasterDataset.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Jared Jacobowitz
+Fall 2021
+SS340 Cause and Effect
+Final Project
+
+Adding fip codes to disaster dataset, simplifying time to just year, and 
+removing unnecessary columns
+"""
+import pandas as pd
+
+# most of the columns are not needed for our analysis
+disaster_data = pd.read_csv("DisasterDeclarationsSummaries.csv",
+                            usecols=("state", 
+                                     "fyDeclared", 
+                                     "incidentType", 
+                                     "fipsStateCode", 
+                                     "fipsCountyCode",
+                                     "designatedArea"))
+
+# combine the state and county fip codes to compare with the temps. dataset
+disaster_data["fips"] = ""
+for row in disaster_data.itertuples():
+    fip = f"{int(row.fipsStateCode):02d}{int(row.fipsCountyCode):03d}"
+    disaster_data.at[row.Index, "fips"] = fip
+    
+# binary data will be easier for applying a disaster to the other dataset
+disaster_data["statewide"] = disaster_data["designatedArea"] == "Statewide"
+
+disaster_data.rename(columns={"fyDeclared":"year"}, inplace=True)
+disaster_data.to_csv("DisasterData.csv", 
+                     columns=("state", 
+                              "year", 
+                              "incidentType", 
+                              "fips", 
+                              "statewide"),
+                     index=False)
