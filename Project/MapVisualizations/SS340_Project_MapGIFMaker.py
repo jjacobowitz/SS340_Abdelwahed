37472926966aaf2d6fc6db230eae74082300de5a
jjacobowitz <57004711+jjacobowitz@users.noreply.github.com>
2021-11-30 22:22:44 -0500

    Create GIFs of the map plots

---
+++ Project/MapVisualizations/SS340_Project_MapGIFMaker.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Jared Jacobowitz
+Fall 2021
+SS340 Cause and Effect
+Final Project
+
+Create gifs of the tempc and disasters US data
+"""
+import imageio
+import glob
+
+for end in ["tempc", "disasters"]:
+    filenames = []
+    for file in glob.glob(f"figures/*{end}.png"):
+        filenames.append(file)
+
+    # copied from: https://stackoverflow.com/a/35943809/12131013
+    images = []
+    for filename in filenames:
+        if "2019" in filename:
+            repeated = [imageio.imread(filename) for _ in range(10)]
+            images.extend(repeated)
+        images.append(imageio.imread(filename))
+    kwargs = {'duration': 0.1}     # frame duration in seconds
+    imageio.mimsave(f'figures/SS350_map_{end}.gif', images, **kwargs)
