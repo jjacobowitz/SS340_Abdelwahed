dca5614064b06c4812a86348da396ec20df43576
jjacobowitz <57004711+jjacobowitz@users.noreply.github.com>
2021-12-02 19:10:34 -0500

    New baseline regression
    The new baseline only includes county fixed effects

--- Project/RegressionCodes/SS340_Project_BaselineRegression.py
+++ Project/RegressionCodes/SS340_Project_BaselineRegression.py
@@ -6,6 +6,11 @@ SS340 Cause and Effect
 Final Project
 
 Baseline regression for the project
+
+Includes county FE
+
+Regression:
+    disasters_{i,t} = b0 + b1 tempc_{i,t} + a_i + e_{i,t}
 """
 import pandas as pd
 import statsmodels.api as sm
@@ -13,16 +18,16 @@ from statsmodels.iolib.summary2 import summary_col
 
 print("Loading data...", end="")
 data = pd.read_csv("../Datasets/CombinedData.csv",
-                    usecols=("fips", "year", "tempc", "disasters"))
+                   usecols=("fips", "year", "tempc", "disasters"))
 print("Done.")
 
 print("Creating endogenous variable...", end="")
 y = data.pop("disasters")
-y = y.to_numpy().reshape(-1,1)
+y = y.to_numpy().reshape(-1, 1)
 print("Done.")
 
 print("Creating Dummies...", end="")
-X = pd.get_dummies(data, columns=["year", "fips"], drop_first=True)
+X = pd.get_dummies(data, columns=["fips"], drop_first=True)
 print("Done.")
 
 print("Running Regression...", end="")
@@ -31,14 +36,14 @@ model = sm.OLS(y, X_sm).fit(cov_type="HC1")
 print("Done.")
 
 print("Saving Summary...", end="")
-stata_summary = summary_col(model, 
-                            stars=True, 
+stata_summary = summary_col(model,
+                            stars=True,
                             float_format="%0.2f",
                             regressor_order=["const", "tempc"],
                             drop_omitted=True)
 
-with open('../Results/baseline_regression_summary.txt', 'w') as f:
-    f.write("Baseline Regression")
+with open('../Results/baseline_regression2_summary.txt', 'w') as f:
+    f.write("Baseline Regression 2")
     f.write('\n')
     f.write(stata_summary.as_text())
 print("Done.")
