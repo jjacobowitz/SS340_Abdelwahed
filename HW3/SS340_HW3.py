f92abd3fee0610b9e15f1eccad8899f81ccdbd78
jjacobowitz <57004711+jjacobowitz@users.noreply.github.com>
2021-12-23 22:17:12 -0500

    Update HW3 folder and code

--- HW3/SS340_HW3.py
+++ HW3/SS340_HW3.py
@@ -8,7 +8,7 @@ Due: 11/01/2021
 
 Python program for Homework 3
 
-Note: The order of operations do not match the HW, so Part 1d might come before
+Note: The order of operations do not match the HW, so Part 1d is done before
 part 1a
 """
 import numpy as np
@@ -25,32 +25,34 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 plt.close("all")
 
 # wipe the summary files
-with open("normal_summary.txt", "w") as f:
+with open("results/normal_summary.txt", "w") as f:
     f.truncate()
 
 # wipe the summary file
-with open("stata_summary.txt", "w") as f:
+with open("results/stata_summary.txt", "w") as f:
     f.truncate()
 
 # =============================================================================
 # Useful Functions
 # =============================================================================
-def run_regression(x, y, 
+
+
+def run_regression(x, y,
                    xlabel=None, ylabel=None, title=None, save_title=None):
     # add the x data to the model
     X = sm.add_constant(x)
-    
+
     # fit the model using robust regression
     model = sm.OLS(y, X).fit(cov_type="HC1")
-    
+
     if None not in [xlabel, ylabel, title, save_title]:
         # add new x data for the plot fit line
         x_cont = np.linspace(x.min(), x.max(), 1000)
         X = sm.add_constant(x_cont)
-        
+
         # generate the regression y data
         y_fit = model.predict(X)
-    
+
         # plot the data with the regression line
         plt.figure()
         plt.scatter(x, y, marker='.', label="data")
@@ -62,30 +64,34 @@ def run_regression(x, y,
         plt.tight_layout()
         plt.show()
         plt.savefig(save_title)
-    
+
     return model, model.params, model.pvalues[1]
+
 
 def get_normal_model_summary(model, title):
     # create the normal-style summary table and save to a txt
     normal_summary = model.summary()
-    with open('normal_summary.txt', 'a') as f:
+    with open('results/normal_summary.txt', 'a') as f:
         f.write(title)
         f.write('\n')
         f.write(normal_summary.as_text())
         f.write("\n\n\n")
-        
+
     return normal_summary
 
-def get_stata_model_summary(model, name, title):    
+
+def get_stata_model_summary(model, name, title):
     # create the stata-style summary table and save to a txt
-    stata_summary = summary_col(model, model_names=name, stars=True, float_format="%0.2f")
-    with open('stata_summary.txt', 'a') as f:
+    stata_summary = summary_col(
+        model, model_names=name, stars=True, float_format="%0.2f")
+    with open('results/stata_summary.txt', 'a') as f:
         f.write(title)
         f.write('\n')
         f.write(stata_summary.as_text())
         f.write("\n\n\n")
-        
+
     return stata_summary
+
 
 def confidence(alpha, single_tailed=True):
     """calculates the single- or double-tailed confidence"""
@@ -93,16 +99,19 @@ def confidence(alpha, single_tailed=True):
         return 1-alpha
     else:
         return 1-(alpha/2)
-    
+
+
 def t_tabulated(conf, dof):
     """calculates the tabulated t-statistic"""
     return t.ppf(conf, dof)
+
 
 def reject_null(p_val, alpha):
     """returns True if the null hypothesis is rejected, False if it fails to be
     rejected. Bases the test on the p-value.
     """
     return p_val <= alpha
+
 
 def null_hypothesis_test(p_val, alpha):
     """runs the null hypothesis test"""
@@ -111,11 +120,12 @@ def null_hypothesis_test(p_val, alpha):
     else:
         print(f"Fail to reject the null hypothesis with alpha = {alpha*100}%")
 
+
 # =============================================================================
 # Read in data
 # =============================================================================
-cols = ["dmage", "mrace3", "dmar", "dlivord", "frace4", "dgestat", "csex", 
-        "dbrwt", "dplural", "cigar", "drink", "wgain", "lung", 
+cols = ["dmage", "mrace3", "dmar", "dlivord", "frace4", "dgestat", "csex",
+        "dbrwt", "dplural", "cigar", "drink", "wgain", "lung",
         "cardiac"]
 
 data = pd.read_csv("pennbirthwgt0.csv", usecols=cols)
@@ -124,26 +134,26 @@ data = pd.read_csv("pennbirthwgt0.csv", usecols=cols)
 # Rename columns (Part 1d)
 # =============================================================================
 # new column names
-rename = ["agemother", "whitemother", "married", "livebirths", 
-          "whitefather", "gestationweek", "male", "birthweight", "kidsatonce", 
-          "cigsperday", "etohperweek", "pregweightgain", "lungdisease", 
+rename = ["agemother", "whitemother", "married", "livebirths",
+          "whitefather", "gestationweek", "male", "birthweight", "kidsatonce",
+          "cigsperday", "etohperweek", "pregweightgain", "lungdisease",
           "cardiacdisease"]
 
-data.rename(columns={old:new for old, new in zip(cols, rename)}, inplace=True)
+data.rename(columns={old: new for old, new in zip(cols, rename)}, inplace=True)
 
 # =============================================================================
 # Remove missing (Parts 1a and 1c)
 # =============================================================================
 # the values for missing data for each variable
-missing = {"livebirths":99,
-           "whitefather":4,
-           "gestationweek":99,
-           "birthweight":9999,
-           "cigsperday":99,
-           "etohperweek":99,
-           "pregweightgain":99,
-           "lungdisease":[8, 9],
-           "cardiacdisease":[8, 9]}
+missing = {"livebirths": 99,
+           "whitefather": 4,
+           "gestationweek": 99,
+           "birthweight": 9999,
+           "cigsperday": 99,
+           "etohperweek": 99,
+           "pregweightgain": 99,
+           "lungdisease": [8, 9],
+           "cardiacdisease": [8, 9]}
 
 # remove rows with missing data
 for k, v in missing.items():
@@ -157,11 +167,11 @@ for k, v in missing.items():
 # Correct dummy variables (Part 1b)
 # =============================================================================
 # convert boolean columns to 1 or 0 by replacing not 1s with 0s
-booleans = ["whitemother", "married", "whitefather", "male", "lungdisease", 
+booleans = ["whitemother", "married", "whitefather", "male", "lungdisease",
             "cardiacdisease"]
 for boolean in booleans:
     data[boolean].replace([2, 3], 0, inplace=True)
-    
+
 data["smoker"] = data["cigsperday"] > 0
 smoker = data["smoker"]         # easier reference to the smoker dummy variable
 
@@ -170,8 +180,8 @@ smoker = data["smoker"]         # easier reference to the smoker dummy variable
 # =============================================================================
 important_statistics = ["mean", "std", "min", "max"]
 data_describe = data.describe().loc[important_statistics].transpose().round(2)
-data_describe[important_statistics].to_csv("SS340_HW3_Describe.csv")
-print(data_describe)    
+data_describe[important_statistics].to_csv("results/SS340_HW3_Describe.csv")
+print(data_describe)
 
 # =============================================================================
 # Mean Difference in Birthweight by Smoking (Parts 2a and 2b)
@@ -185,15 +195,15 @@ print(f"Mean birthweight smoker: {mean_birthweight_smoker:.2f}g")
 
 # hypothesis: average birthweight is similar for smokers and non-smokers
 # H0: smoker_mean_birthweight - nonsmoker_mean_birthweight = 0
-# Ha: smoker_mean_birthweight - nonsmoker_mean_birthweight != 0   
+# Ha: smoker_mean_birthweight - nonsmoker_mean_birthweight != 0
 alpha = 0.05
 conf = confidence(alpha, single_tailed=False)
 n = len(data)
 dof = n - 2     # -2 because there are two means used
 
 # mean-mean null hypothesis
-t_stat, p_val = ttest_ind(data["birthweight"][~smoker], 
-                          data["birthweight"][smoker], 
+t_stat, p_val = ttest_ind(data["birthweight"][~smoker],
+                          data["birthweight"][smoker],
                           equal_var=False)
 
 print("\n")
@@ -206,20 +216,20 @@ print(f"{t_stat=:.2f}, {t_tab=:.2f}, {p_val=:.2f}")
 # Table of differences (Part 2c)
 # =============================================================================
 # variables we think might show the populations of mothers are not comparable
-of_interest = ["birthweight", "agemother", "whitemother", "married", 
-               "livebirths", "gestationweek", "kidsatonce", "etohperweek", 
+of_interest = ["birthweight", "agemother", "whitemother", "married",
+               "livebirths", "gestationweek", "kidsatonce", "etohperweek",
                "pregweightgain", "cardiacdisease"]
 
 # skeleton of the table with the desired columns
 table_of_diffs = pd.DataFrame(columns=["Mean",
-                                       "Smoker", 
-                                       "Not Smoker", 
-                                       "Difference", 
+                                       "Smoker",
+                                       "Not Smoker",
+                                       "Difference",
                                        "p-value"])
 
 table_of_diffs.index.name = "Variable"  # rename the index column to variable
 
-# Creates a table showing the values for each group, their differences, and 
+# Creates a table showing the values for each group, their differences, and
 # the p-value, where the null hypothesis is that they are equal. If the null
 # hypothesis is rejected (p<0.05) then there is a difference, and the groups
 # are not equal
@@ -231,14 +241,14 @@ for interest in of_interest:
     nonsmoker_result = nonsmoker_data.mean()
     diff = smoker_result - nonsmoker_result
     _, p_value = ttest_ind(smoker_data, nonsmoker_data, equal_var=False)
-    row = {"Mean":mean,
-           "Smoker":smoker_result, 
-           "Not Smoker":nonsmoker_result, 
-           "Difference":diff, 
-           "p-value":round(p_value,2)}  # round to 2 decimals for clarity
+    row = {"Mean": mean,
+           "Smoker": smoker_result,
+           "Not Smoker": nonsmoker_result,
+           "Difference": diff,
+           "p-value": round(p_value, 2)}  # round to 2 decimals for clarity
     row = pd.Series(row, name=interest)
     table_of_diffs = table_of_diffs.append(row)
-table_of_diffs.to_csv("SS340_HW3_TableofDiffs.csv")
+table_of_diffs.to_csv("results/SS340_HW3_TableofDiffs.csv")
 
 # =============================================================================
 # Correlation matrix (Part 2d)
@@ -248,7 +258,7 @@ corr_matrix = data[of_interest].corr()
 print("\n")
 print(corr_matrix.round(2))
 
-# OVB is a table where the birthweight and cigsperday columns are the 
+# OVB is a table where the birthweight and cigsperday columns are the
 # correlation values with the variable and the sign column is the sign of the
 # OVB based on the correlations
 # probably a better way to do this..
@@ -259,21 +269,23 @@ OVB = pd.DataFrame(columns=["birthweight", "cigsperday", "sign"])
 # fill the table with the values
 for interest in of_interest[1:]:
     sign = 1    # will be the sign of the OVB
-    
+
     # add the rows without data
-    row = pd.Series({"birthweight":np.nan, "cigsperday":np.nan, "sign":np.nan},
+    row = pd.Series({"birthweight": np.nan,
+                     "cigsperday": np.nan,
+                     "sign": np.nan},
                     name=interest)
     OVB = OVB.append(row)
-    
-    # calculate the signs 
+
+    # calculate the signs
     for col in OVB.columns:
         if col != "sign":
-            corr = data[[col, interest]].corr().round(2).to_numpy()[0,1]
+            corr = data[[col, interest]].corr().round(2).to_numpy()[0, 1]
             OVB[col][interest] = corr
             sign *= np.sign(corr)
         else:
             OVB[col][interest] = sign
-OVB.to_csv("SS340_HW3_OVB.csv")
+OVB.to_csv("results/SS340_HW3_OVB.csv")
 
 # =============================================================================
 # Simple Regression (Part 2e)
@@ -287,7 +299,7 @@ result = run_regression(data["cigsperday"],
                         xlabel=xlabel,
                         ylabel=ylabel,
                         title=title,
-                        save_title="SS340_HW3_simpleregress.png")
+                        save_title="figures/SS340_HW3_simpleregress.png")
 
 model, [beta_0, beta_1], p_val = result
 normal_summary = get_normal_model_summary(model, title)
@@ -301,19 +313,16 @@ null_hypothesis_test(p_val, alpha)
 # =============================================================================
 # Adding more covariates (Part 2f)
 # =============================================================================
-covariates = ["cigsperday", 
+covariates = ["cigsperday",
               "married",
               "livebirths",
               "gestationweek",
-              "whitemother",                  # until here: good controls
-              "agemother", 
-              "whitefather", 
-              "male",  
-              "kidsatonce",
-               # "etohperweek",
-               # "lungdisease", 
-               # "cardiacdisease",                 # until here: useless controls
-               "pregweightgain",]                # until here: bad controls
+              "whitemother",            # until here: good controls
+              "agemother",
+              "whitefather",
+              "male",
+              "kidsatonce",             # until here: useless controls
+              "pregweightgain", ]       # until here: bad controls
 models = []
 names = []
 for indx, covariate in enumerate(covariates):
